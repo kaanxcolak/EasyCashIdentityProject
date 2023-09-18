0dd35883dce7356c65c9b939ac460eddfc8bf2b1
Kaan Çolak <kaancolak3458@gmail.com>
2023-09-18 10:33:17 +0300

    added Migration

---
+++ EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,24 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Concrete
+{
+    public class Context:IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-P4SDEGD;initial catalog=EasyCashDb;integrated Security=true");
+        }
+
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }  //C# tarafındaki Sınıf ismi --- sql serverdaki tablo ismi
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+
+    }
+}
