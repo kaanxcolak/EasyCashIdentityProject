2101c468f5d7cf326d0a9c6617b6ec44a529003a
Kaan Çolak <kaancolak3458@gmail.com>
2023-09-19 08:41:42 +0300

    some validations changes

--- EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccount.cs
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccount.cs
@@ -14,8 +14,10 @@ namespace EasyCashIdentityProject.EntityLayer.Concrete
         public string CustomerAccountNumber { get; set; }
         public string CustomerAccountCurrency { get; set; }
         public decimal CustomerAccountBalance { get; set; }
-        public string BankBranch { get; set; }  
+        public string BankBranch { get; set; }
 
+        public int AppUserID { get; set; }
+        public AppUser AppUser { get; set; }
 
     }
 }
