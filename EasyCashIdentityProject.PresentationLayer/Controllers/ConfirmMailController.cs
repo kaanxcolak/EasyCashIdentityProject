1597c2d2edbeaf9b7c0a8d377ec3d5812017e32b
Kaan Çolak <kaancolak3458@gmail.com>
2023-09-24 20:07:06 +0300

    updated

--- EasyCashIdentityProject.PresentationLayer/Controllers/ConfirmMailController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/ConfirmMailController.cs
@@ -1,23 +1,38 @@
-﻿using EasyCashIdentityProject.PresentationLayer.Models;
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using EasyCashIdentityProject.PresentationLayer.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EasyCashIdentityProject.PresentationLayer.Controllers
 {
-	public class ConfirmMailController : Controller
-	{
-		[HttpGet]
-		public IActionResult Index(int id)
-		{
-			var value = TempData["Mail"];
-			ViewBag.v = value + " aaa";
-			return View();
-		}
+    public class ConfirmMailController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
 
-		[HttpPost]
-		public IActionResult Index(ConfirmMailViewModel confirmMailViewModel)
-		{
-			return View();
+        public ConfirmMailController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
 
-		}
-	}
+        [HttpGet]
+        public IActionResult Index(int id)
+        {
+            var value = TempData["Mail"];
+            ViewBag.v = value;  
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(ConfirmMailViewModel confirmMailViewModel)
+        {
+            var value = TempData["Mail"];
+            var user = await _userManager.FindByEmailAsync(value.ToString());
+            if (user.ConfirmCode == confirmMailViewModel.ConfirmCode)
+            {
+                return RedirectToAction("Index","MyProfile");     
+            }
+            return View();
+
+        }
+    }
 }
