e601c97553e2f700b98a0b2f6adc94735da04679
Kaan Çolak <kaancolak3458@gmail.com>
2023-09-26 09:51:31 +0300

    added Customer Layouts

---
+++ EasyCashIdentityProject.PresentationLayer/ViewComponents/Customer/_CustomerLayoutHeaderPartial.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Plugins;
+using System.ComponentModel;
+
+namespace EasyCashIdentityProject.PresentationLayer.ViewComponents.Customer
+{
+//    Partial class, bir sınıfın tanımını birden fazla dosyaya bölerek daha düzenli bir yapı oluşturmayı sağlar.Bu, büyük ve karmaşık sınıfların daha kolay yönetilmesini sağlar.
+
+//View Component ise tekrar kullanılabilir bileşenlerin oluşturulmasına yardımcı olan bir yapıdır. Controller'a bağlı olmayan ve bağımsız olarak çalışabilen bileşenlerdir.
+
+    public class _CustomerLayoutHeaderPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
