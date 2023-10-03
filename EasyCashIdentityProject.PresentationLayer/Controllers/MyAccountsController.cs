8f4bc295953443a552c4c6f46471eac50cf18c18
Kaan Çolak <kaancolak3458@gmail.com>
2023-10-03 08:41:15 +0300

    updated

--- EasyCashIdentityProject.PresentationLayer/Controllers/MyAccountsController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/MyAccountsController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EasyCashIdentityProject.PresentationLayer.Controllers
 {
+    [Authorize]
     public class MyAccountsController : Controller
     {
         public IActionResult Index()
