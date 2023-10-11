1f5c8c8e69ae153f575e788bd49aecb9c12cfeed
Kaan Çolak <kaancolak3458@gmail.com>
2023-10-11 17:55:49 +0300

    updated

---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/SendMoneyController.cs
@@ -0,0 +1,41 @@
+﻿using EasyCashIdentityProject.DataAccessLayer.Concrete;
+using EasyCashIdentityProject.DtoLayer.Dtos.CustomerAccountProcessDtos;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+    public class SendMoneyController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public SendMoneyController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(SendMoneyForCustomerAccountProcessDto sendMoneyForCustomerAccountProcessDto)
+        {
+            var context = new Context();
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            sendMoneyForCustomerAccountProcessDto.SenderID = user.Id;
+            sendMoneyForCustomerAccountProcessDto.ProcessDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            sendMoneyForCustomerAccountProcessDto.ProcessType = "Havale";
+            
+            var receiverAccountNumber = context.CustomerAccounts.Where(x=>x.CustomerAccountNumber ==
+            sendMoneyForCustomerAccountProcessDto.ReceiverAccountNumber).Select(y=>y.CustomerAccountID).FirstOrDefault();   
+            
+
+            return View();
+        }
+    }
+}
