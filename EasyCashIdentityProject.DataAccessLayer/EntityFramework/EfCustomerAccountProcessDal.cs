7e963b19277c381da1e2bf4986ff4cc957693db4
Kaan Çolak <kaancolak3458@gmail.com>
2024-03-22 00:36:27 +0300

    updated

--- EasyCashIdentityProject.DataAccessLayer/EntityFramework/EfCustomerAccountProcessDal.cs
+++ EasyCashIdentityProject.DataAccessLayer/EntityFramework/EfCustomerAccountProcessDal.cs
@@ -2,6 +2,7 @@
 using EasyCashIdentityProject.DataAccessLayer.Concrete;
 using EasyCashIdentityProject.DataAccessLayer.Repositories;
 using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +16,8 @@ namespace EasyCashIdentityProject.DataAccessLayer.EntityFramework
         public List<CustomerAccountProcess> MyLastProcess(int id)
         {
             using var context = new Context();
-            var values = context.CustomerAccountProcesses.Where(x => x.ReceiverID == id || x.SenderID == id).ToList();
+            var values = context.CustomerAccountProcesses.Include(y=>y.SenderCustomer).Include(w=>w.ReceiverCustomer).ThenInclude(z=>z.AppUser).Where(x => 
+            x.ReceiverID == id || x.SenderID == id).ToList();
             return values;
         }
     }
